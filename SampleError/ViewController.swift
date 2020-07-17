afe2c847b4c244d8cf8c6140ee97ade188de9904
TakumaNakamura <nakamuratakuma@MacBook-Pro-4.local>
2020-07-18 02:12:14 +0900

    first commit

---
+++ SampleError/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleError
+//
+//  Created by 中村拓馬 on 2020/06/13.
+//  Copyright © 2020 Nakamuratakuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var label :UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "こんにちは"
+    }
+
+
+}
+
